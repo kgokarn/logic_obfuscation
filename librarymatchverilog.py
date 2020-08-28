0c2ab0d9945b8131376859a68f5e7805d60fb026
kgokarn <kartik.gokarn@gmail.com>
2020-08-28 11:04:26 +0200

    Two, three and four input of most gates are handled.

--- librarymatchverilog.py
+++ librarymatchverilog.py
@@ -2,7 +2,7 @@ import sys
 
 
 def main():
-    verilog_ptr = open("almost_correct_adder16_netlist.v")
+    verilog_ptr = open("traditional_multiplier16_netlist.v")
     while verilog_ptr:
         read_line = verilog_ptr.readline()
 
@@ -22,6 +22,44 @@ def main():
             verilog_file.write(line_parser)
             continue
 
+        if "XOR3_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(","")
+            line_parser = line_parser.replace(".B(","")
+            line_parser = line_parser.replace(".B1(","")
+            line_parser = line_parser.replace(".B2(","")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".A1(","")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".Z(","")
+            line_parser = line_parser.replace(".ZN(","")
+            line_parser = line_parser.replace("),",",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("XOR3_X1", "xor")
+            verilog_file.write(line_parser)
+            continue
+
+        if "XOR4_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(","")
+            line_parser = line_parser.replace(".B(","")
+            line_parser = line_parser.replace(".B1(","")
+            line_parser = line_parser.replace(".B2(","")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".B4(", "")
+            line_parser = line_parser.replace(".A1(","")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".A4(", "")
+            line_parser = line_parser.replace(".Z(","")
+            line_parser = line_parser.replace(".ZN(","")
+            line_parser = line_parser.replace("),",",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("XOR4_X1", "xor")
+            verilog_file.write(line_parser)
+            continue
+
         if "XNOR2_X1" in read_line:
             line_parser = read_line
             line_parser = line_parser.replace(".A(", "")
@@ -35,6 +73,44 @@ def main():
             line_parser = line_parser.replace("),", ",")
             line_parser = line_parser.replace(") ", "")
             line_parser = line_parser.replace("XNOR2_X1", "xnor")
+            verilog_file.write(line_parser)
+            continue
+
+        if "XNOR3_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(", "")
+            line_parser = line_parser.replace(".B(", "")
+            line_parser = line_parser.replace(".B1(", "")
+            line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".A1(", "")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".Z(", "")
+            line_parser = line_parser.replace(".ZN(", "")
+            line_parser = line_parser.replace("),", ",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("XNOR3_X1", "xnor")
+            verilog_file.write(line_parser)
+            continue
+
+        if "XNOR4_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(", "")
+            line_parser = line_parser.replace(".B(", "")
+            line_parser = line_parser.replace(".B1(", "")
+            line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".B4(", "")
+            line_parser = line_parser.replace(".A1(", "")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".A4(", "")
+            line_parser = line_parser.replace(".Z(", "")
+            line_parser = line_parser.replace(".ZN(", "")
+            line_parser = line_parser.replace("),", ",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("XNOR4_X1", "xnor")
             verilog_file.write(line_parser)
             continue
 
@@ -54,21 +130,44 @@ def main():
             verilog_file.write(line_parser)
             continue
 
-        if "XNOR2_X1" in read_line:
+        if "NOR3_X1" in read_line:
             line_parser = read_line
             line_parser = line_parser.replace(".A(", "")
             line_parser = line_parser.replace(".B(", "")
             line_parser = line_parser.replace(".B1(", "")
             line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".B3(", "")
             line_parser = line_parser.replace(".A1(", "")
             line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
             line_parser = line_parser.replace(".Z(", "")
             line_parser = line_parser.replace(".ZN(", "")
             line_parser = line_parser.replace("),", ",")
             line_parser = line_parser.replace(") ", "")
-            line_parser = line_parser.replace("XNOR2_X1", "xnor")
+            line_parser = line_parser.replace("NOR3_X1", "nor")
             verilog_file.write(line_parser)
             continue
+
+        if "NOR4_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(", "")
+            line_parser = line_parser.replace(".B(", "")
+            line_parser = line_parser.replace(".B1(", "")
+            line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".B4(", "")
+            line_parser = line_parser.replace(".A1(", "")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".A4(", "")
+            line_parser = line_parser.replace(".Z(", "")
+            line_parser = line_parser.replace(".ZN(", "")
+            line_parser = line_parser.replace("),", ",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("NOR4_X1", "nor")
+            verilog_file.write(line_parser)
+            continue
+
 
         if "INV_X1" in read_line:
             line_parser = read_line
@@ -85,6 +184,23 @@ def main():
             line_parser = line_parser.replace("INV_X1", "not")
             verilog_file.write(line_parser)
             continue
+
+        if "INV_X2" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(", "")
+            line_parser = line_parser.replace(".B(", "")
+            line_parser = line_parser.replace(".B1(", "")
+            line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".A1(", "")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".Z(", "")
+            line_parser = line_parser.replace(".ZN(", "")
+            line_parser = line_parser.replace("),", ",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("INV_X2", "not")
+            verilog_file.write(line_parser)
+            continue
+
 
         if "NAND2_X1" in read_line:
             line_parser = read_line
@@ -177,6 +293,26 @@ def main():
             verilog_file.write(line_parser)
             continue
 
+        if "AND4_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(", "")
+            line_parser = line_parser.replace(".B(", "")
+            line_parser = line_parser.replace(".B1(", "")
+            line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".B4(", "")
+            line_parser = line_parser.replace(".A1(", "")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".A4(", "")
+            line_parser = line_parser.replace(".Z(", "")
+            line_parser = line_parser.replace(".ZN(", "")
+            line_parser = line_parser.replace("),", ",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("AND4_X1", "and")
+            verilog_file.write(line_parser)
+            continue
+
 
         if "OAI21_X1" in read_line:
             line_parser = read_line
@@ -226,6 +362,45 @@ def main():
             verilog_file.write(line_parser)
             continue
 
+        if "OR3_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(", "")
+            line_parser = line_parser.replace(".B(", "")
+            line_parser = line_parser.replace(".B1(", "")
+            line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".A1(", "")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".Z(", "")
+            line_parser = line_parser.replace(".ZN(", "")
+            line_parser = line_parser.replace("),", ",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("OR3_X1", "or")
+            verilog_file.write(line_parser)
+            continue
+
+        if "OR4_X1" in read_line:
+            line_parser = read_line
+            line_parser = line_parser.replace(".A(", "")
+            line_parser = line_parser.replace(".B(", "")
+            line_parser = line_parser.replace(".B1(", "")
+            line_parser = line_parser.replace(".B2(", "")
+            line_parser = line_parser.replace(".B3(", "")
+            line_parser = line_parser.replace(".B4(", "")
+            line_parser = line_parser.replace(".A1(", "")
+            line_parser = line_parser.replace(".A2(", "")
+            line_parser = line_parser.replace(".A3(", "")
+            line_parser = line_parser.replace(".A4(", "")
+            line_parser = line_parser.replace(".Z(", "")
+            line_parser = line_parser.replace(".ZN(", "")
+            line_parser = line_parser.replace("),", ",")
+            line_parser = line_parser.replace(") ", "")
+            line_parser = line_parser.replace("OR4_X1", "or")
+            verilog_file.write(line_parser)
+            continue
+
+
         if "endmodule" in read_line:
             line_parser = read_line
             verilog_file.write(line_parser)
@@ -240,6 +415,6 @@ def main():
 
 
 if __name__ == '__main__':
-    verilog_file = open("almost_correct_adder16_net.v", "w+")
+    verilog_file = open("traditional_multiplier16.v", "w+")
     main()
     sys.exit(0)
