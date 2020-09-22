2f5e7a654d373baf61add0aa9ee86f45e9693c1d
kgokarn <kartik.gokarn@gmail.com>
2020-09-22 21:40:24 +0200

    simulation of 32 keygate xor locked wallace tree multiplier netlist

---
+++ lockednetlistsimulation/32xor/wallace_tree_multiplier/sim/simulation/traditional_multiplier8_tb/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity traditional_multiplier8_tb is
+end traditional_multiplier8_tb;
---
+++ lockednetlistsimulation/32xor/wallace_tree_multiplier/sim/simulation/tradtional_multiplier8_xor_enc32/_primary.vhd
@@ -0,0 +1,10 @@
+library verilog;
+use verilog.vl_types.all;
+entity tradtional_multiplier8_xor_enc32 is
+    port(
+        op1_i           : in     vl_logic_vector(7 downto 0);
+        op2_i           : in     vl_logic_vector(7 downto 0);
+        keyinput        : in     vl_logic_vector(31 downto 0);
+        product_o       : out    vl_logic_vector(15 downto 0)
+    );
+end tradtional_multiplier8_xor_enc32;
